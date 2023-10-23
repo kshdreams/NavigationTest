7eb77e9c116d634f8bc9832f59cd8759f394f2d5
Danny Kim <ssm_sebiya@samsung.com>
2023-10-23 11:13:24 +0900

    [feat] add code to reproduce compose + AndroidView(RecyclerView) scroll problem after navigate

--- app/src/main/java/com/test/navigation/presentation/MainActivity.kt
+++ app/src/main/java/com/test/navigation/presentation/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.wear.compose.material.Button
+import androidx.compose.ui.text.style.TextAlign
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
@@ -27,25 +31,41 @@ class MainActivity : ComponentActivity() {
             val nav = rememberSwipeDismissableNavController()
             SwipeDismissableNavHost(
                 navController = nav,
-                startDestination = "first" // "second" working fine, but "first" does not work
+                startDestination = "A" // "B" working well, but "A" does not work
             ) {
-                composable("first") {
-                    BoxWithConstraints(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Button(
-                            modifier = Modifier.align(Alignment.Center),
-                            onClick = {
-                                //                    nav.popBackStack()
-                                nav.navigate("second")
-                            }
-                        ) {
-                            Text("click")
+                composable("A") {
+                    ScalingLazyColumn(modifier = Modifier.fillMaxSize()) {
+                        item {
+                            Chip(
+                                onClick = {
+                                    nav.navigate("B")
+                                },
+                                label = {
+                                    Text(modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, text = "navigate to B")
+                                },
+                                secondaryLabel = {
+                                    Text(modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, text = "scroll problem")
+                                }
+                            )
+                        }
+                        item {
+                            // scroll will work even though startDestination is "A"
+                            Chip(
+                                onClick = {
+                                    nav.popBackStack()
+                                    nav.navigate("B")
+                                },
+                                label = {
+                                    Text(modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, text = "navigate to B")
+                                },
+                                secondaryLabel = {
+                                    Text(modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, text = "with popupBackStack")
+                                }
+                            )
                         }
                     }
-
                 }
-                composable("second") {
+                composable("B") {
                     BoxWithConstraints(
                         modifier = Modifier.fillMaxSize()
                     ) {
